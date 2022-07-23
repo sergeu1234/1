c07041a1a89e7c991c6970e61c4cb4a98c2ecbec
John Doe <johndoe@example.com>
2022-07-23 18:36:11 +0300

    hhggh

--- wert/settings.py
+++ wert/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'wert.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
